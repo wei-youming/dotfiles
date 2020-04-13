fbd15f0d2cd28bec395934c724e84fbce1bf0e41
wei-youming <weiym16@lzu.edu.cn>
2020-04-13 16:58:29 +0800

    commit

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -4,8 +4,7 @@ mv ~/.config/sxhkd/sxhkdrc{,.bak}
 mv ~/.config/nvim/init.vim{,.bak}
 ln -sb ~/dotfiles/bspwm/bspwmrc ~/.config/bspwm/
 ln -sb ~/dotfiles/sxhkd/sxhkdrc  ~/.config/sxhkd/
-ln -sb ~/dotfiles/vim/vimrc ~/.vimrc
-ln -sb ~/dotfiles/vim/vimrc ~/.vimrc
-ln -sb ~/dotfiles/zshrc ~/.zshrc
+ln -sb ~/dotfiles/vim/basic.vim ~/.config/nvim/init.vim
+ln -sb ~/dotfiles/zsh/zshrc ~/.zshrc
 
 
